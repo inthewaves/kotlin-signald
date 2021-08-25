b6ee40b85c0e059d8ba454f7d5728358723a259f
inthewaves <inthewaves@pm.me>
2021-08-25 03:12:24 -0700

    flesh out Node.js support

--- client/src/jsMain/kotlin/org/inthewaves/kotlinsignald/Sockets.kt
+++ client/src/jsMain/kotlin/org/inthewaves/kotlinsignald/Sockets.kt
@@ -2,96 +2,116 @@ package org.inthewaves.kotlinsignald
 
 import Buffer
 import NodeJS.get
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withTimeout
 import net.Socket
 import org.inthewaves.kotlinsignald.clientprotocol.SuspendSocketCommunicator
 import process
-import kotlin.js.Promise
+import kotlin.coroutines.resumeWithException
 
 private const val NOT_SUPPORTED_ERROR_MSG = "not supported in JavaScript"
 
-private inline fun createErrorListener(
-    crossinline handleException: (SocketUnavailableException) -> Unit
-) = { error: Any ->
-    @Suppress("ThrowableNotThrown")
-    val exception =  if (error is Throwable) {
-        SocketUnavailableException(error)
-    } else {
-        SocketUnavailableException("$error")
-    }
-    handleException(exception)
-}
+private typealias ErrorHandler = (Any) -> Unit
 
-private fun makeNewSocketConnection(socketPath: String): Promise<Socket> {
-     return Promise<Socket> { resolve, reject ->
-         val socket = net.createConnection(socketPath)
-         val errorListener = createErrorListener { exception ->
-             socket.destroy()
-             reject(exception)
-         }
-         // Add a listener for data so that we drop the version line
-         socket
-             .once("error", errorListener)
-             .once("data") { _: Buffer ->
-                 socket.removeListener("error", errorListener)
-                 resolve(socket)
-             }
+private class NodeSocket private constructor(private val rawSocket: Socket, val socketPath: String) {
+    val throwingListener = createErrorListenerWithWrappedError {
+        rawSocket.destroy()
+        throw it
     }
-}
 
-private fun getSocketOrThrow(socketPath: String?): Promise<Pair<String, Socket>> {
-    if (socketPath != null) {
-        return makeNewSocketConnection(socketPath).then { socketPath to it }
-    } else {
-        val promises = getDefaultSocketPaths().toList()
-            .map { defaultPathToTry ->
-                makeNewSocketConnection(defaultPathToTry)
-                    .then(onFulfilled = { defaultPathToTry to it }, onRejected = { null })
+    init {
+        rawSocket.apply {
+            removeAllListeners("error")
+            addListener("error", throwingListener)
+        }
+    }
+
+    fun close() {
+        rawSocket.destroy()
+    }
+
+    suspend fun writeAndReadReply(request: String): String {
+        return suspendCancellableCoroutine { cont ->
+            rawSocket.apply {
+                write("$request\n")
+                once("data") { buffer: Buffer -> cont.resume("$buffer", null) }
             }
-            .toTypedArray()
-
-        return Promise.all(promises).then { array ->
-            val nonNull = array.firstOrNull { it != null }
-            // If there were multiple successes, only pick the first one and close the other connections
-            array.forEach { if (it != null && it != nonNull) it.second.destroy() }
-            return@then nonNull ?: throw SocketUnavailableException("unable")
         }
     }
 
-    /*
-    for (currentPath in socketPathsToTry) {
-        val socket = makeNewSocketConnection(currentPath).then(onFulfilled = { it }, onRejected = { null }).await()
-        if (socket != null) {
-            return Promise.resolve(currentPath to socket)
+    suspend fun readLine(): String? {
+        return withTimeout(2000L) {
+            suspendCancellableCoroutine { cont ->
+                val lineReader = { buffer: Buffer -> cont.resume("$buffer", null) }
+                rawSocket.once("data", lineReader)
+            }
         }
     }
 
-    return Promise.reject(
-        SocketUnavailableException(
-            if (socketPath != null) "tried $socketPath but wasn't available" else "tried default paths but not available"
-        )
-    )
-     */
-}
+    companion object {
+        private inline fun createErrorListenerWithWrappedError(
+            crossinline handleException: (wrapped: SocketUnavailableException) -> Unit
+        ): ErrorHandler = { error: Any ->
+            if (error is CancellationException) throw error
+            @Suppress("ThrowableNotThrown")
+            val exception = if (error is Throwable) {
+                SocketUnavailableException(error)
+            } else {
+                SocketUnavailableException("$error")
+            }
+            handleException(exception)
+        }
 
-private fun readLineFromSocket(socket: Socket): Promise<String> {
-    return Promise<String> { resolve, reject ->
-        socket
-            .once("data") { buffer: Buffer -> resolve("$buffer") }
+        private suspend fun makeNewNetSocketConnection(socketPath: String): Socket {
+            return suspendCancellableCoroutine { cont ->
+                @Suppress("JoinDeclarationAndAssignment")
+                lateinit var socket: Socket
+                val errorListener = createErrorListenerWithWrappedError { wrapped ->
+                    socket.destroy()
+                    cont.resumeWithException(wrapped)
+                }
+                socket = net.createConnection(socketPath)
+                socket.once("error", errorListener)
+                    .once("data") { _: Buffer ->
+                        // ignore version line
+                        val throwingListener = createErrorListenerWithWrappedError { throw it }
+                        socket.addListener("error", throwingListener)
+                        socket.removeListener("error", errorListener)
+                        cont.resume(socket, onCancellation = { socket.destroy() })
+                    }
+            }
+        }
+
+        /**
+         * @throws SocketUnavailableException
+         */
+        suspend fun create(socketPath: String?): NodeSocket {
+            if (socketPath != null) {
+                return NodeSocket(makeNewNetSocketConnection(socketPath), socketPath)
+            }
+
+            return getDefaultSocketPaths()
+                .firstNotNullOfOrNull { defaultPathToTest ->
+                    try {
+                        NodeSocket(makeNewNetSocketConnection(defaultPathToTest), defaultPathToTest)
+                    } catch (e: SocketUnavailableException) {
+                        null
+                    }
+                }
+                ?: throw SocketUnavailableException("unable to make a connection from default socket paths")
+        }
     }
-    TODO("SOCK")
 }
 
 public actual class SocketWrapper private constructor(
     public actual val actualSocketPath: String
 ) : SuspendSocketCommunicator {
-
-    override suspend fun submitSuspend(request: String): String {
-        TODO("Not yet implemented")
+    override suspend fun submitSuspend(request: String): String = useNewSocketConnection { socket ->
+        socket.writeAndReadReply(request)
     }
 
-    override suspend fun readLineSuspend(): String? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun readLineSuspend(): String? = useNewSocketConnection { socket -> socket.readLine() }
 
     override fun submit(request: String): String {
         throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
@@ -101,29 +121,34 @@ public actual class SocketWrapper private constructor(
         throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
     }
 
+    private suspend inline fun <T> useNewSocketConnection(block: (socket: NodeSocket) -> T): T {
+        val socket = NodeSocket.create(actualSocketPath)
+        try {
+            return block(socket)
+        } finally {
+            socket.close()
+        }
+    }
+
     public actual companion object {
         public actual fun create(socketPath: String?): SocketWrapper {
             throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
         }
 
-        public fun createAsync(socketPath: String?): Promise<SocketWrapper> {
-            return getSocketOrThrow(socketPath)
-                .then { (goodSocketPath, socket) ->
-                    socket.destroy()
-                    SocketWrapper(goodSocketPath)
-                }
+        public suspend fun createAsync(socketPath: String?): SocketWrapper {
+            val nodeSocket = NodeSocket.create(socketPath)
+            nodeSocket.close()
+            return SocketWrapper(nodeSocket.socketPath)
         }
     }
 }
 
-public actual class PersistentSocketWrapper private constructor(private val socket: Socket) : SuspendSocketCommunicator {
-    override suspend fun submitSuspend(request: String): String {
-        TODO("Not yet implemented")
-    }
+public actual class PersistentSocketWrapper private constructor(
+    private val socket: NodeSocket
+) : SuspendSocketCommunicator {
+    override suspend fun submitSuspend(request: String): String = socket.writeAndReadReply(request)
 
-    override suspend fun readLineSuspend(): String? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun readLineSuspend(): String? = socket.readLine()
 
     override fun submit(request: String): String {
         throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
@@ -134,7 +159,7 @@ public actual class PersistentSocketWrapper private constructor(private val sock
     }
 
     public actual fun close() {
-        throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
+        socket.close()
     }
 
     public actual companion object {
@@ -142,9 +167,8 @@ public actual class PersistentSocketWrapper private constructor(private val sock
             throw UnsupportedOperationException(NOT_SUPPORTED_ERROR_MSG)
         }
 
-        public fun createAsync(socketPath: String?): Promise<PersistentSocketWrapper> {
-            return getSocketOrThrow(socketPath)
-                .then { (_, socket) -> PersistentSocketWrapper(socket) }
+        public suspend fun createAsync(socketPath: String?): PersistentSocketWrapper {
+            return PersistentSocketWrapper(NodeSocket.create(socketPath))
         }
     }
 }
