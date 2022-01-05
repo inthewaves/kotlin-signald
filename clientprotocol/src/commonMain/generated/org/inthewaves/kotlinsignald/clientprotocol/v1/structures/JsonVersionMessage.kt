c83cf9210ba9fe28cc0d56e5da995e0da5028794
inthewaves <inthewaves@pm.me>
2022-01-05 13:29:02 -0800

    clientprotocol: update to signald 0.15.0-43-cd821ab5

--- clientprotocol/src/commonMain/generated/org/inthewaves/kotlinsignald/clientprotocol/v1/structures/JsonVersionMessage.kt
+++ clientprotocol/src/commonMain/generated/org/inthewaves/kotlinsignald/clientprotocol/v1/structures/JsonVersionMessage.kt
@@ -10,7 +10,7 @@ public data class JsonVersionMessage(
      */
     public val name: String? = null,
     /**
-     * Example: "0.15.0-31-7354fec1"
+     * Example: "0.15.0-43-cd821ab5"
      */
     public val version: String? = null,
     /**
@@ -18,7 +18,7 @@ public data class JsonVersionMessage(
      */
     public val branch: String? = null,
     /**
-     * Example: "7354fec1d4233224e6827869f64d95ea06981205"
+     * Example: "cd821ab56766c1024540710f404a9727666c5c78"
      */
     public val commit: String? = null
 ) : SignaldResponseBodyV1()
