6f48a612fc307c08e44af25f826bb627e3e4f499
inthewaves <inthewaves@pm.me>
2023-05-17 16:42:33 -0700

    bump version to 0.30.0+signald-0.23.2-7-1583b4df

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
 
 allprojects {
     group = "org.inthewaves.kotlin-signald"
-    version = "0.29.1+signald-0.23.2-7-1583b4df"
+    version = "0.30.0+signald-0.23.2-7-1583b4df"
 
     repositories {
         mavenCentral()
